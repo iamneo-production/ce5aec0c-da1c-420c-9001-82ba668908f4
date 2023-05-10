f8058604eb72f93ceca31b00949a922cff019201
krish0809 <krishna2220000@gmail.com>
2023-05-10 15:33:53 +0530

    updated the base template

---
+++ Backend/Admission-Portal/src/test/java/com/examly/Admission/Portal/AdmissionPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examly.Admission.Portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmissionPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
