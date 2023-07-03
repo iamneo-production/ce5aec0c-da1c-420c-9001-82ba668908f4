3daed93e04fc41fcc5c0c81a36824fb61e91c617
krish0809 <krishna2220000@gmail.com>
2023-07-03 10:54:17 +0000

    Updated Course Repo

--- springapp/src/main/java/com/examly/springapp/repository/CourseRepo.java
+++ springapp/src/main/java/com/examly/springapp/repository/CourseRepo.java
@@ -1,3 +1,8 @@
-public class CourseRepo {
+package com.examly.springapp.repository;
+
+import com.examly.springapp.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepo extends JpaRepository<Course,Long>{
     
 }
