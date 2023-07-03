8a90af07edb0206b11d9dcaec80ac6740bc6ac11
Shrinidhi-12 <shrinidhid6@gmail.com>
2023-07-03 13:00:48 +0000

    Updated the student repo and course service files

--- springapp/src/main/java/com/examly/springapp/service/CourseService.java
+++ springapp/src/main/java/com/examly/springapp/service/CourseService.java
@@ -1,3 +1,10 @@
-public class CourseService {
-    
-}
+package com.examly.springapp.service;
+
+import java.util.List;
+
+import com.examly.springapp.repository.CourseRepo;
+import com.examly.springapp.model.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
