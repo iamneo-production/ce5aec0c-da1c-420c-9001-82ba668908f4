256321b97d24a6cf64e53163858ff5205dd7a181
NithyaU25 <nithyauthiran12@gmail.com>
2023-07-03 11:33:32 +0000

    updated the files in the model package

--- springapp/src/main/java/com/examly/springapp/model/Student.java
+++ springapp/src/main/java/com/examly/springapp/model/Student.java
@@ -1,3 +1,31 @@
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.springframework.stereotype.Component;
+
+@Table(name = "student", schema = "admissionportal")
+@Entity
+@Component
 public class Student {
+    @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    private String phoneNumber;
+
+    public Student() {
+    }
     
-}
