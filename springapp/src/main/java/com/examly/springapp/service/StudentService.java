f511c1b46d806d98bb0c749f65246d93919e13f4
Muk3812 <mukeshvaishnav6111@gmail.com>
2023-07-03 13:28:45 +0000

    done with completing the student service file

--- springapp/src/main/java/com/examly/springapp/service/StudentService.java
+++ springapp/src/main/java/com/examly/springapp/service/StudentService.java
@@ -7,3 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+@Service
+public class StudentService {
+    @Autowired
+    private final StudentRepo studentRepo;
+
+    @Autowired
+    public StudentService(StudentRepo studentRepo) {
+        this.studentRepo = studentRepo;
+    }
+
+    public Student insertStudent(Student student) {
+        return studentRepo.save(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepo.findAll();
+    }
+    public Student getStudnetById(long id) {
+		return studentRepo.findById(id).get();
+	}
+
+    public boolean loginUser(Student student) {
+        Student user = studentRepo.findByEmailAndPassword(
+                student.getEmail(), student.getPassword());
+        return user != null;
+    }
+}
