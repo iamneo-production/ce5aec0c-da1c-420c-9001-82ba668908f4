622d66321f22fc74b2af150386baab51d1f6670f
Muk3812 <mukeshvaishnav6111@gmail.com>
2023-07-28 05:13:29 +0000

    eidtsometing

--- admission-portal/src/Components/UserProfile.jsx
+++ admission-portal/src/Components/UserProfile.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { useContext } from 'react';
+import React, { useState, useEffect, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import '../CSS/UserProfile.css';
